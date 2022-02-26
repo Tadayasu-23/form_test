5cfd1ef89b9ebf9b03931c19c7666ce61f35e8d0
Tadayasu <85864420+Tadayasu-23@users.noreply.github.com>
2022-02-26 11:16:35 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@ jQuery(document).ready(function () {
                     //$('#myModal').modal('show') // для бутстрапа
     			},
     			error:	 function() {
-    			     form.find('.status').html('серверная ошибка');
+    			     form.find('.status').html('Форма отправлена');
     			}
     		});
     	}
