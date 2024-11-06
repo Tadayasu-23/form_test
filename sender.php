28009dae606bb4517ec7331bc09504db3d5c38ad
Tadayasu <85864420+Tadayasu-23@users.noreply.github.com>
2024-11-06 21:28:06 +0300

    Update sender.php

--- sender.php
+++ sender.php
@@ -1,6 +1,8 @@
 <?php
     $name = $_POST['name'];
 	$phone = $_POST['phone'];
+	$mail = $_POST['mail'];
+	$mesg = $_POST['msg'];
 
 	$to = "Tadayasu@yandex.ru"; 
 	$date = date ("d.m.Y"); 
@@ -12,6 +14,8 @@
 	$msg="
     Имя: $name
     Телефон: $phone
+    Почта: $mail
+    Сообщение: $mesg
 	mail($to, $subject, $msg, "From: $from ");
 
 ?>
