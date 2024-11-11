f4a5f37e66eed4e69d5f2a0e12b89327c436fafd
Tadayasu <85864420+Tadayasu-23@users.noreply.github.com>
2024-11-11 14:46:26 +0300

    Add files via upload

--- sender.php
+++ sender.php
@@ -1,27 +1,58 @@
 <?php
-	$question = $_POST['topic'];
-   $name = $_POST['name'];
-	$phone = $_POST['phone'];
-	$email = $_POST['email'];
-	$address = $_POST['address'];
-	$text = $_POST['text'];
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\Exception;
 
-	$to = "Tadayasu@yandex.ru"; 
-	$date = date ("d.m.Y"); 
-	$time = date ("h:i");
-	$from = "AllienWalker@mail.ru";
-	$subject = "Заявка c сайта";
+	require 'php-mailer-master\src\PHPMailer.php';
+	require 'php-mailer-master\src\Exception.php';
 
-	
-	$msg="
-	Цель обращения: $question
-   Имя: $name
-   Контактный номер телефона: $phone
-	Электронная почта: $email
-	Адресс: $address
-	Сообщение: $text
-	mail($to, $subject, $msg, "From: $from ");
+	$mail = new PHPMailer(true);
+	$mail->CharSet = 'UTD-8';
+	$mail->setLanguage('ru', 'phpmailer/language/');
+	$mail->IsHTML(true);
 
+	//От кого
+	$mail->setForm('Yukino23@yandex.ru', 'Обратная связь для родителей');
+	//Кому
+	$mail->addAddress('Tadayasu@yandex.ru');
+	//Тема
+	$mail->Subject = 'Обратная связь для родителей';
+
+	//Тело письма
+	$body = '<h1>Встречайте супер письмо!<h1>';
+
+	$body = '<p><strong>Тема обращения:</strong> '.$_POST['topic'].'</p>';
+	if(trim(!empty($_POST['name']))){
+		$body = '<p><strong>Имя:</strong> '.$_POST['name'].'</p>';
+	}
+	if(trim(!empty($_POST['phone']))){
+		$body = '<p><strong>Телефон:</strong> '.$_POST['phone'].'</p>';
+	}
+	if(trim(!empty($_POST['email']))){
+		$body = '<p><strong>Электронный адресс:</strong> '.$_POST['email'].'</p>';
+	}
+	if(trim(!empty($_POST['address']))){
+		$body = '<p><strong>Адресс:</strong> '.$_POST['address'].'</p>';
+	}
+	if(trim(!empty($_POST['text']))){
+		$body = '<p><strong>Сообщение:</strong> '.$_POST['text'].'</p>';
+	}
+
+	$mail->Body = $body;
+
+	//Отправка
+	if (!$mail->send()) {
+		$message = 'Ошибка';
+	} else
+ {
+	$message = 'Данные отправлены';
+ }
+
+ $response = [
+	'message' => $message
+ ];
+
+ header('Content-type: application/json');
+ echo json_encode($response);
 ?>
 
 <p>Привет, форма отправлена</p>
